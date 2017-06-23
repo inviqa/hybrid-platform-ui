6596b24dc971db71b9da13a66d25cea642333e67
Max Baldanza <mbaldanza@inviqa.com>
2017-06-23 14:24:45 +0100

    EZP-27443: Display main content tab

---
+++ src/lib/Repository/Values/Content/UiContentType.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\HybridPlatformUi\Repository\Values\Content;
+
+use eZ\Publish\API\Repository\Values\ContentType\ContentType as APIContentType;
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use eZ\Publish\Core\Repository\Values\ContentType\ContentType;
+
+/**
+ * Extends original value object in order to provide functionality.
+ */
+class UiContentType extends ContentType
+{
+    public function __construct(APIContentType $contentType)
+    {
+        parent::__construct(get_object_vars($contentType));
+    }
+
+    /**
+     * Gets the content type field definitions based on the field group.
+     * If no field group is provided it will return all field definitions that have an empty field group.
+     *
+     * @param string|null $fieldGroup
+     *
+     * @return FieldDefinition[]
+     */
+    public function getFieldDefinitionsByFieldGroup($fieldGroup = null)
+    {
+        $fieldDefinitions = [];
+        foreach ($this->getFieldDefinitions() as $fieldDefinition) {
+            if ($fieldGroup == $fieldDefinition->fieldGroup) {
+                $fieldDefinitions[] = $fieldDefinition;
+            }
+        }
+
+        return $fieldDefinitions;
+    }
+}
